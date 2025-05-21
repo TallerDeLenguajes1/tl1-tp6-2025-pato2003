ff02aef13bf314df2e9437999fc59763de1252cb
pato2003 <patogabrielferreyra@gmail.com>
2025-05-21 00:33:32 -0300

    Algunos cambios

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -1,4 +1,102 @@
-﻿Console.WriteLine("Ingrese el numero que desee invertir: ");
-int num = int.Parse(Console.ReadLine());
-float inverso = (float)1 / num;
-Console.WriteLine(inverso);
+﻿bool exit = true;
+while (exit)
+{
+    int num1;
+    int num2;
+    bool opcion1;
+    bool opcion2;
+    Console.WriteLine("-------------Menu-------------\n1) Sumar\n2) Restar\n3) Multiplicar\n4) Dividir\n5) Salir\n------------------------------\nElija la operacion: ");
+    int opcion;
+    
+    if (!int.TryParse(Console.ReadLine(), out opcion))
+    {
+        Console.WriteLine("No se ingresó un numero");
+    }
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Ingrese el primer numero: ");
+            opcion1 = int.TryParse(Console.ReadLine(), out num1);
+            if (!opcion1)
+            {
+                Console.WriteLine("No se ingresó un numero");
+                break;
+            }
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            opcion2 = int.TryParse(Console.ReadLine(), out num2);
+            if (!opcion2)
+            {
+                Console.WriteLine("No se ingresó un numero");
+                break;
+            }
+
+            Console.WriteLine("RESULTADO = " + (num1 + num2));
+            break;
+
+        case 2:
+            Console.WriteLine("Ingrese el primer numero: ");
+            opcion1 = int.TryParse(Console.ReadLine(), out num1);
+            if (!opcion1)
+            {
+                Console.WriteLine("No se ingresó un numero");
+                break;
+            }
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            opcion2 = int.TryParse(Console.ReadLine(), out num2);
+            if (!opcion2)
+            {
+                Console.WriteLine("No se ingresó un numero");
+                break;
+            }
+
+            Console.WriteLine("RESULTADO = " + (num1 - num2));
+            break;
+
+        case 3:
+            Console.WriteLine("Ingrese el primer numero: ");
+            opcion1 = int.TryParse(Console.ReadLine(), out num1);
+            if (!opcion1)
+            {
+                Console.WriteLine("No se ingresó un numero");
+                break;
+            }
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            opcion2 = int.TryParse(Console.ReadLine(), out num2);
+            if (!opcion2)
+            {
+                Console.WriteLine("No se ingresó un numero");
+                break;
+            }
+
+            Console.WriteLine("RESULTADO = " + (num1 * num2));
+            break;
+
+        case 4:
+            Console.WriteLine("Ingrese el primer numero: ");
+            opcion1 = int.TryParse(Console.ReadLine(), out num1);
+            if (!opcion1)
+            {
+                Console.WriteLine("No se ingresó un numero");
+                break;
+            }
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            opcion2 = int.TryParse(Console.ReadLine(), out num2);
+            if (!opcion2)
+            {
+                Console.WriteLine("No se ingresó un numero");
+                break;
+            }
+
+            Console.WriteLine("RESULTADO = " + (float)(num1 / num2));
+            break;
+
+        default:
+            exit = false;
+            break;
+    }
+    
+}
