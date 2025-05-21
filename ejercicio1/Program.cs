9e93825e788ead9e8fd8677f3d060e1b9a524f67
pato2003 <patogabrielferreyra@gmail.com>
2025-05-21 00:48:15 -0300

    Realizacion de las funciones utilizando Math

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -1,4 +1,32 @@
-﻿Console.WriteLine("Ingrese el numero que desee invertir: ");
-int num = int.Parse(Console.ReadLine());
-float inverso = (float)1 / num;
-Console.WriteLine(inverso);
+﻿int num;
+Console.WriteLine("Ingrese un numero: ");
+bool opcion = int.TryParse(Console.ReadLine(), out num);
+if (!opcion)
+{
+    Console.WriteLine("No se ingreso un numero valido");
+}
+
+Console.WriteLine(Math.Abs(num));
+Console.WriteLine(Math.Pow(num, 2));
+Console.WriteLine(Math.Sqrt(num));
+Console.WriteLine(Math.Sin(num));
+Console.WriteLine(Math.Cos(num));
+Console.WriteLine(Math.Truncate((float)num));
+
+
+int num1, num2;
+Console.WriteLine("Ingrese un nuevo numero: ");
+opcion = int.TryParse(Console.ReadLine(), out num1);
+if (!opcion)
+{
+    Console.WriteLine("No se ingreso un numero valido");
+}
+
+Console.WriteLine("Ingrese un nuevo numero: ");
+opcion = int.TryParse(Console.ReadLine(), out num2);
+if (!opcion)
+{
+    Console.WriteLine("No se ingreso un numero valido");
+}
+
+Console.WriteLine("Maximo: " + Math.Max(num1, num2) + "\nMinimo: " + Math.Min(num1,num2));
