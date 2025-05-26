4dbbad226930bc54e78f238fe5e21ce91f5310ca
pato2003 <patogabrielferreyra@gmail.com>
2025-05-26 19:24:24 -0300

    Parte dos termindada

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -6,23 +6,23 @@ if (!opcion)
     Console.WriteLine("No se ingreso un numero valido");
 }
 
-Console.WriteLine(Math.Abs(num));
-Console.WriteLine(Math.Pow(num, 2));
-Console.WriteLine(Math.Sqrt(num));
-Console.WriteLine(Math.Sin(num));
-Console.WriteLine(Math.Cos(num));
-Console.WriteLine(Math.Truncate((float)num));
+Console.WriteLine("Valor Absoluto: " + Math.Abs(num));
+Console.WriteLine("Cuadrado: " + Math.Pow(num, 2));
+Console.WriteLine("Raiz Cuadrada: " + Math.Sqrt(num));
+Console.WriteLine("Seno: " + Math.Sin(num));
+Console.WriteLine("Coseno: " + Math.Cos(num));
+Console.WriteLine("Parte Real: " + Math.Truncate((float)num));
 
 
 int num1, num2;
-Console.WriteLine("Ingrese un nuevo numero: ");
+Console.WriteLine("\n\n\nIngrese 2 numeros:\n\nIngrese el primer numero: ");
 opcion = int.TryParse(Console.ReadLine(), out num1);
 if (!opcion)
 {
     Console.WriteLine("No se ingreso un numero valido");
 }
 
-Console.WriteLine("Ingrese un nuevo numero: ");
+Console.WriteLine("Ingrese el segundo numero: ");
 opcion = int.TryParse(Console.ReadLine(), out num2);
 if (!opcion)
 {
